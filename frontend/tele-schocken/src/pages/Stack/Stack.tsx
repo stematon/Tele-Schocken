8e25e23aec3e66202eae0fcf26684159d193900a
Skill3t <larslengersdorf@gmail.com>
2020-03-27 19:43:40 +0100

    add empty stack

---
+++ frontend/tele-schocken/src/pages/Stack/Stack.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+
+interface StackProps{
+}
+@observer
+export class Stack extends React.Component<StackProps> {
+
+render(){
+return(
+    <div></div>
+    );
+}
+}
